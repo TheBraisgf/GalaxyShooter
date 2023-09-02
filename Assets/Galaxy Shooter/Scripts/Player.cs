c8e71a8d4f1c59d650f8a8e0821005a489b5d4b6
braisgf <braisgf@gmail.com>
2023-09-02 19:10:30 +0200

    Laser Behaviour

--- Assets/Galaxy Shooter/Scripts/Player.cs
+++ Assets/Galaxy Shooter/Scripts/Player.cs
@@ -10,7 +10,17 @@ public class Player : MonoBehaviour
 //Option value assigned
 
 [SerializeField]
-private float speed = 5.0f;
+private GameObject _laserPrefab;
+
+[SerializeField]
+private float _fireRate = 0.25f;
+private float _canFire = 0.0f;
+
+
+[SerializeField]
+private float _speed = 5.0f;
+
+
 
 
  // Start is called before the first frame update
@@ -25,18 +35,35 @@ void Start()
 void Update()
 {
 Movement();
+
+//If key pressed spawn laser at player position
+if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+{
+Shoot();
+}
+
 }
 
 
 //Methods
+
+private void Shoot()
+{
+    if(Time.time > _canFire)
+    {
+    //Spawn laser
+    Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.88f, 0), Quaternion.identity);
+    _canFire = Time.time + _fireRate;
+    }
+}
 
 private void Movement()
 {
     float horizontalInput = Input.GetAxis("Horizontal");
     float verticalInput = Input.GetAxis("Vertical");
 
-    transform.Translate(Vector3.right  * speed * horizontalInput * Time.deltaTime);
-    transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+    transform.Translate(Vector3.right  * _speed * horizontalInput * Time.deltaTime);
+    transform.Translate(Vector3.up * _speed * verticalInput * Time.deltaTime);
 
      // If player on the y is greater than0 set player position 0
     if(transform.position.y > 0){
